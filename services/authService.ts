31161478058aa197da30ff7a35664996120ba9d4
dhariane <dharianeylan@gmail.com>
2026-06-18 16:03:32 +0300

    misokatra ilay admin

--- services/authService.ts
+++ services/authService.ts
@@ -1,18 +1,4 @@
-import axios from 'axios';
-import api, { handleApiError } from './api';
-import { User, Admin } from '@/types';
-
-// Interface temporaire pour mapper la réponse de votre Django avec les tokens à la racine
-interface BackendAuthResponse {
-  access?: string;
-  refresh?: string;
-  [key: string]: any; 
-}
-interface RegisterResult {
-  status: number;
-  success: boolean;
-  message: string;
-}
+import api from './api'
 
 const authService = {
   login: async (email: string, password: string) => {
@@ -82,43 +68,5 @@ const authService = {
     }
   },
 }
-export const adminRegister = async (
-  username: string,
-  password: string,
-  email: string,
-  nom: string,
-  prenom: string,
-): Promise<RegisterResult> => {
-  try {
-    const formData = new FormData();
-    formData.append('username', username);
-    formData.append('email', email);
-    formData.append('password', password);
-    formData.append('nom', nom);
-    formData.append('prenom', prenom);
-
-    await api.post(
-      `/auth/admin/register/`, 
-      formData, 
-      { headers: { 'Content-Type': 'multipart/form-data' } }
-    );
-
-    return { status: 201, success: true, message: "Profil enregistré" };
-
-  } catch (error: any) {
-      if (axios.isAxiosError(error) && error.response) {
-      const status = error.response.status;
-      const result = error.response.data;
-
-
-      return {
-        status: status,
-        success: false,
-        message: result.message,
-      };
-    }
-    return { status: 500, success: false, message: "Erreur de connexion au serveur" };
-  }
-};
 
 export default authService
